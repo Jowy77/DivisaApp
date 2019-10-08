1b1a8cb446e413cd45b485af39a86a4043153ddc
Jowy77 <Iproyectz@hotmail.com>
2019-10-08 10:27:49 +0100

    first commit

--- src/main/java/dad/DivisaApp/DivisaApp.java
+++ src/main/java/dad/DivisaApp/DivisaApp.java
@@ -6,8 +6,6 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -35,17 +33,17 @@ public class DivisaApp extends Application {
 		
 		selectorDivisa1 = new ComboBox<String>();
 		selectorDivisa1.getItems().addAll("Euro","Dolar","Yen","Libra");
-		selectorDivisa1.setPromptText("Euro");
+		selectorDivisa1.getSelectionModel().selectFirst();
 		
 		selectorDivisa2 = new ComboBox<String>();
 		selectorDivisa2.getItems().addAll("Euro","Dolar","Yen","Libra");
-		selectorDivisa2.setPromptText("Dolares");
+		selectorDivisa2.setValue("Dolar");
 		
 		changeButton = new Button("Change");
 		changeButton.setDefaultButton(true);
 		changeButton.setOnAction(e-> onLoginButtonAction(e));
-
 		
+
 		HBox divisa1Box= new HBox(5,cantidadDinero1,selectorDivisa1);
 		divisa1Box.setAlignment(Pos.CENTER);
 		
@@ -65,15 +63,89 @@ public class DivisaApp extends Application {
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
+		
 	}
 	
 	public static void main (String [] args) {
 		launch(args);
 	}
 
-	private Object onLoginButtonAction(ActionEvent e) {
-		// TODO Auto-generated method stub
-		return null;
+	private void onLoginButtonAction(ActionEvent e) {
+		
+		Divisa euro = new Divisa("Euro", 1.0);
+		Divisa libra = new Divisa("Libra", 0.8873);
+		Divisa dolar = new Divisa("Dolar", 1.2007);
+		Divisa yen = new Divisa("Yen", 133.59);
+		String divisaOrigen = selectorDivisa1.getValue();
+		String divisaDestino = selectorDivisa2.getValue();
+		String cantidad = cantidadDinero1.getText();
+		String dineroConvertido;
+		
+		if(divisaOrigen == "Euro") {
+			switch(divisaDestino) {
+			case "Euro":  dineroConvertido = String.valueOf(euro.fromEuro(euro.toEuro(Double.parseDouble(cantidad))));
+						  cantidadDinero2.setText(dineroConvertido); 
+						  break;
+			case "Dolar": dineroConvertido = String.valueOf(dolar.fromEuro(euro.toEuro(Double.parseDouble(cantidad))));
+			 			  cantidadDinero2.setText(dineroConvertido); 
+			 			  break;
+			case "Libra": dineroConvertido = String.valueOf(libra.fromEuro(euro.toEuro(Double.parseDouble(cantidad))));
+						  cantidadDinero2.setText(dineroConvertido);
+						  break;
+			case "Yen":   dineroConvertido = String.valueOf(yen.fromEuro(euro.toEuro(Double.parseDouble(cantidad))));
+			 			  cantidadDinero2.setText(dineroConvertido);
+			 			  break;
+			}
+		}else if(divisaOrigen == "Dolar") {
+			switch(divisaDestino) {
+			case "Euro":  dineroConvertido = String.valueOf(euro.fromEuro(dolar.toEuro(Double.parseDouble(cantidad))));
+						  cantidadDinero2.setText(dineroConvertido); 
+						  break;
+			case "Dolar": dineroConvertido = String.valueOf(dolar.fromEuro(dolar.toEuro(Double.parseDouble(cantidad))));
+			 			  cantidadDinero2.setText(dineroConvertido); 
+			 			  break;
+			case "Libra": dineroConvertido = String.valueOf(libra.fromEuro(dolar.toEuro(Double.parseDouble(cantidad))));
+						  cantidadDinero2.setText(dineroConvertido);
+						  break;
+			case "Yen":   dineroConvertido = String.valueOf(yen.fromEuro(dolar.toEuro(Double.parseDouble(cantidad))));
+			 			  cantidadDinero2.setText(dineroConvertido);
+			 			  break;
+			}
+			
+		}else if(divisaOrigen == "Libra") {
+			switch(divisaDestino) {
+			case "Euro":  dineroConvertido = String.valueOf(euro.fromEuro(libra.toEuro(Double.parseDouble(cantidad))));
+						  cantidadDinero2.setText(dineroConvertido); 
+						  break;
+			case "Dolar": dineroConvertido = String.valueOf(dolar.fromEuro(libra.toEuro(Double.parseDouble(cantidad))));
+			 			  cantidadDinero2.setText(dineroConvertido); 
+			 			  break;
+			case "Libra": dineroConvertido = String.valueOf(libra.fromEuro(libra.toEuro(Double.parseDouble(cantidad))));
+						  cantidadDinero2.setText(dineroConvertido);
+						  break;
+			case "Yen":   dineroConvertido = String.valueOf(yen.fromEuro(libra.toEuro(Double.parseDouble(cantidad))));
+			 			  cantidadDinero2.setText(dineroConvertido);
+			 			  break;
+			}
+			
+		}else {
+			switch(divisaDestino) {
+			case "Euro":  dineroConvertido = String.valueOf(euro.fromEuro(yen.toEuro(Double.parseDouble(cantidad))));
+						  cantidadDinero2.setText(dineroConvertido); 
+						  break;
+			case "Dolar": dineroConvertido = String.valueOf(dolar.fromEuro(yen.toEuro(Double.parseDouble(cantidad))));
+			 			  cantidadDinero2.setText(dineroConvertido); 
+			 			  break;
+			case "Libra": dineroConvertido = String.valueOf(libra.fromEuro(yen.toEuro(Double.parseDouble(cantidad))));
+						  cantidadDinero2.setText(dineroConvertido);
+						  break;
+			case "Yen":   dineroConvertido = String.valueOf(yen.fromEuro(yen.toEuro(Double.parseDouble(cantidad))));
+			 			  cantidadDinero2.setText(dineroConvertido);
+			 			  break;
+			}
+		}
+		
+		
 	}
 
 }
